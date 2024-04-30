33dd60bfd700b033b04ed905f6ec26a476105ff8
gigasamkharadze <samkharadze.giga@kiu.edu.ge>
2024-04-30 19:45:29 +0400

    feat: categories view for the product admin

--- products/admin.py
+++ products/admin.py
@@ -5,8 +5,8 @@ from products.models import Product, Category
 # Register your models here.
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price', 'amount', 'total_price')
-    list_filter = ('price', 'amount')
+    list_display = ('title', 'price', 'amount', 'total_price', 'display_categories')
+    list_filter = ('price', 'amount', 'categories')
     search_fields = ('title',)
     list_per_page = 10
     ordering = ('price', 'amount')
@@ -14,6 +14,10 @@ class ProductAdmin(admin.ModelAdmin):
     def total_price(self, obj):
         return obj.price * obj.amount
     total_price.short_description = 'Total price'
+
+    def display_categories(self, obj):
+        return ', '.join([category.title for category in obj.categories.all()])
+    display_categories.short_description = 'Categories'
 
 
 @admin.register(Category)
